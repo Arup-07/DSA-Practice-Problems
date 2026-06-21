2b3614b4ddced3cebc32492e761b6f196cd15c22
Arup Jyoti <arupjyotimohanty7710@gmail.com>
2026-06-21 23:52:19 +0530

    Q8

---
+++ q8.java
@@ -0,0 +1,9 @@
+/*Convert an integer to string without using any inbuilt method */
+class q8{
+    public static void main(String args[])
+    {
+        int n=123456;
+        String s=n+"";
+        System.out.println(s);
+    }
+} 
