94d4ae0157e07e0588faa71ff3c9f1e9d04f5c36
Arup Jyoti <arupjyotimohanty7710@gmail.com>
2026-07-02 00:00:51 +0530

    Q24

--- q24.java
+++ q24.java
@@ -1,7 +1,18 @@
-/*
+/*Leetcode 14. Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
  */
 
-class Solution {
+class q24 {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        q24 obj = new q24();
+        String result = obj.longestCommonPrefix(strs);
+        System.out.println(result);
+    }
     public String longestCommonPrefix(String[] strs) {
         if(strs==null || strs.length==0)
             return "";
