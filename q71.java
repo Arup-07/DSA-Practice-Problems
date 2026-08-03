6261914997e53c058196f1e165e308d0f04d93d3
Arup Jyoti <arupjyotimohanty7710@gmail.com>
2026-08-03 23:38:17 +0530

    LeetCode 7

---
+++ q71.java
@@ -0,0 +1,34 @@
+/*LeetCode 7:Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+Example 1:
+Input: x = 123
+Output: 321
+ */
+
+
+
+import java.util.*;
+public class q71 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        q71 obj = new q71();
+        int result = obj.reverse(x);
+        System.out.println(result);
+    }
+    public int reverse(int x) {
+        int rev=0;
+        while(x!=0)
+        {
+            int r=x%10;
+            x/=10;
+            if(rev>Integer.MAX_VALUE/10 || (rev==Integer.MAX_VALUE/10 && r>7))
+                return 0;
+            if(rev<Integer.MIN_VALUE/10 || (rev==Integer.MIN_VALUE/10 && r<-8))
+                return 0;
+            rev=rev*10+r;
+        }
+        return rev;
+    }
+}
