5a3e5f0fdb6c63264cbe8942502bb97411d0838e
Arup Jyoti <arupjyotimohanty7710@gmail.com>
2026-08-21 00:35:56 +0530

    LeetCode 3069

--- q89.java
+++ q89.java
@@ -9,6 +9,33 @@ Example 1:
 Input: nums = [2,1,3]
 Output: [2,3,1]
 */
-
-
-import java.util.*;
+class q89 {
+    public static void main(String args[])
+    {
+        q89 ob= new q89();
+        int nums[]= {2,1,3};
+        int res[]=ob.resultArray(nums);
+        for(int i=0;i<res.length;i++)
+            System.out.print(res[i]+" ");
+    }
+    public int[] resultArray(int[] nums) {
+        int l=nums.length,p=1,q=1;
+        int a[]= new int[l];
+        int b[]= new int[l];
+        a[0]= nums[0];
+        b[0]=nums[1];
+        for(int i=2;i<l;i++)
+        {
+            if(a[p-1]>b[q-1])
+                a[p++]=nums[i];
+            else
+                b[q++]=nums[i];
+        }       
+        int res[]=  new int[l];
+        for(int i=0;i<p;i++)
+            res[i]=a[i];
+        for(int i=p,j=0;i<l;i++)
+            res[i]=b[j++];
+        return res;
+    }
+}
